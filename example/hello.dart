a1294943d692495d904d98d637249dcb48a5d02c
projectcuan <87933353+projectcuan@users.noreply.github.com>
2022-04-07 13:47:42 +0700

    dart library

---
+++ example/hello.dart
@@ -0,0 +1,9 @@
+import 'package:dart_packages_library/hello.dart';
+
+void main() {
+  print(sayHello("Doni"));
+
+  var customer = Customer();
+  var product = Product();
+  var category = Category();
+}
